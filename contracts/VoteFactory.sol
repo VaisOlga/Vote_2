02ff4fcfb780c7188c72c803944107f3dea29439
Olga <15_3_vays@mail.ru>
2018-07-17 15:27:03 +0500

    add voting

--- contracts/VoteFactory.sol
+++ contracts/VoteFactory.sol
@@ -1,13 +1,15 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
 
 import "./Ownable.sol";
 
-contract VoteFactory is Ownable{
+contract VoteFactory is Ownable {
     
     struct Vote {
         string question;
         string[] answers;
         State state;
+        address[] voters;
+        mapping (address => uint) voterToAnswer;
     }
 
     enum State {
@@ -19,6 +21,11 @@ contract VoteFactory is Ownable{
     Vote[] public votes;
     mapping (uint => address) voteToOwner;
     
+
+    modifier onlyVoteOwner(uint _voteId) {
+        require(voteToOwner[_voteId] == msg.sender);
+        _;
+    }
 
     modifier voteInitial(uint _voteId) {
         require(votes[_voteId].state == State.Initial);
@@ -37,24 +44,85 @@ contract VoteFactory is Ownable{
 
 
     function createVote(string _question) public {
-        uint voteId = votes.push(Vote(_question, new string[](0), State.Initial)) - 1;
+        uint voteId = votes.push(Vote(_question, new string[](0), State.Initial, new address[](0))) - 1;
         voteToOwner[voteId] = msg.sender;
     }
+
+    function setQuestion(uint _voteId, string _question)
+    public
+    onlyVoteOwner(_voteId)
+    voteInitial(_voteId) {
+        votes[_voteId].question = _question;
+    }
     
-    function addAnswer(uint _voteId, string _answer) public voteInitial(_voteId) {
+    function addAnswer(uint _voteId, string _answer)
+    public
+    onlyVoteOwner(_voteId)
+    voteInitial(_voteId) {
         require(voteToOwner[_voteId] == msg.sender);
         votes[_voteId].answers.push(_answer);
     }
 
-    function startVote(uint _voteId) public voteInitial(_voteId) {
-        require(voteToOwner[_voteId] == msg.sender);
+    function setAnswer(uint _voteId, uint _answerId, string _answer)
+    public
+    onlyVoteOwner(_voteId)
+    voteInitial(_voteId) {
+        votes[_voteId].answers[_answerId] = _answer;
+    }
+
+    function startVote(uint _voteId)
+    public
+    onlyVoteOwner(_voteId)
+    voteInitial(_voteId) {
         require(votes[_voteId].answers.length > 1);
         votes[_voteId].state = State.Started;
     }
 
-    function stopVote(uint _voteId) public voteStarted(_voteId) {
-        require(voteToOwner[_voteId] == msg.sender);
+    function stopVote(uint _voteId)
+    public
+    onlyVoteOwner(_voteId)
+    voteStarted(_voteId) {
         votes[_voteId].state = State.Stopped;
+    }
+    
+    function cast(uint _voteId, uint _answerId) public voteStarted(_voteId) {
+        for (uint i = 0; i < votes[_voteId].voters.length; i++) {
+            if (votes[_voteId].voters[i] == msg.sender) {
+                return;
+            }
+        }
+        votes[_voteId].voterToAnswer[msg.sender] = _answerId;
+        votes[_voteId].voters.push(msg.sender);
+    }
+
+    function _results(uint _voteId) internal view returns(uint, uint) {
+        require(votes[_voteId].voters.length > 0);
+        uint[] memory counts = new uint[](votes[_voteId].voters.length);
+        for (uint i = 0; i < votes[_voteId].voters.length; i++) {
+            counts[votes[_voteId].voterToAnswer[votes[_voteId].voters[i]]]++;
+        }
+        uint index = 0;
+        uint count = 0;
+        for (i = 0; i < counts.length; i++) {
+            if (counts[i] > counts[index]) {
+                index = i;
+                count = 0;
+            }
+            if (counts[i] == counts[index]) {
+                count++;
+            }
+        }
+        return (index, count);
+    }
+
+    function results(uint _voteId) public view returns(string) {
+        uint index;
+        uint count;
+        (index, count) = _results(_voteId);
+        if (count > 1) {
+            return "It is impossible to determine, because multiple voting leaders";
+        }
+        return votes[_voteId].answers[index];
     }
 
 
@@ -64,5 +132,21 @@ contract VoteFactory is Ownable{
 
     function voteAnswer(uint _voteId, uint _answerId) public view returns(string) {
         return votes[_voteId].answers[_answerId];
+    }
+
+    function voteState(uint _voteId) public view returns(State) {
+        return votes[_voteId].state;
+    }
+
+    function contractOwner() public view returns(address) {
+        return owner;
+    }
+
+    function voterAnswerId(address _voter, uint _voteId) public view returns(uint) {
+        return votes[_voteId].voterToAnswer[_voter];
+    }
+
+    function voterAddress(uint _voteId, uint _voterId) public view returns(address) {
+        return votes[_voteId].voters[_voterId];
     }
 }
--- test/VoteFactory.js
+++ test/VoteFactory.js
@@ -17,11 +17,18 @@ contract('VoteFactory', function(accounts) {
     const owner = accounts[0];
     const creator = accounts[1];
     const user = accounts[2];
+    const user2 = accounts[3];
+    const user3 = accounts[4];
+    const user4 = accounts[5];
 
     const question0 = "Question 0";
+    const question1 = "Question 1";
     const answer0 = "Answer 0";
     const answer1 = "Answer 1";
     const answer2 = "Answer 2"
+    const one = "1";
+    const zero = "0";
+    const multipleLiders = "It is impossible to determine, because multiple voting leaders";
 
     beforeEach('setup contract for each test', async function () {
         voteFactory = await VoteFactory.new({from: owner});
@@ -33,21 +40,16 @@ contract('VoteFactory', function(accounts) {
         vote.should.be.equal(question0);
     });
 
-    it('answer added by creator', async function () {
-        await voteFactory.createVote(question0, {from: creator});
-        await voteFactory.addAnswer(0, answer0, {from: creator});
-    });
-
-    it('answer added by user', async function () {
-        await voteFactory.createVote(question0, {from: creator});
-        await expectThrow(voteFactory.addAnswer(0, answer0, {from: user}));
-    });
-
-    it('correct answer added by creator', async function() {
+    it('vote initial: answer added by creator', async function() {
         await voteFactory.createVote(question0, {from: creator});
         await voteFactory.addAnswer(0, answer0, {from: creator});
         var vote = await voteFactory.voteAnswer(0, 0);
         vote.should.be.equal(answer0);
+    });
+
+    it('(ET) vote initial: answer added by user', async function () {
+        await voteFactory.createVote(question0, {from: creator});
+        await expectThrow(voteFactory.addAnswer(0, answer0, {from: user}));
     });
 
     it('vote initial -> started -> stopped', async function() {
@@ -58,22 +60,22 @@ contract('VoteFactory', function(accounts) {
         await voteFactory.stopVote(0, {from: creator});
     });
 
-    if('vote do not started else answers < 2', async function() {
+    if('(ET) vote started: answers < 2', async function() {
         await voteFactory.createVote(question0, {from: creator});
         await voteFactory.addAnswer(0, answer0, {from: creator});
         await expectThrow(voteFactory.startVote(0, {from:creator}));
     });
 
-    it('vote stopped when vote do not initial', async function() {
+    it('(ET) vote stopped: vote do not initial', async function() {
         await expectThrow(voteFactory.stopVote(0, {from: creator}));
     });
 
-    it('vote stopped when vote do not started', async function() {
+    it('(ET) vote stopped: vote do not started', async function() {
         await voteFactory.createVote(question0, {from: creator});
         await expectThrow(voteFactory.stopVote(0, {from: creator}));
     });
 
-    it('answer added when vote started', async function() {
+    it('(ET) vote started: add answer', async function() {
         await voteFactory.createVote(question0, {from: creator});
         await voteFactory.addAnswer(0, answer0, {from: creator});
         await voteFactory.addAnswer(0, answer1, {from: creator});
@@ -81,7 +83,154 @@ contract('VoteFactory', function(accounts) {
         await expectThrow(voteFactory.addAnswer(0, answer2, {from: creator}));
     });
 
-    it('transfer ownership', async function() {
+    it('transfer ownership by owner', async function() {
         await voteFactory.transferOwnership(user, {from: owner});
+        var newOwner = await voteFactory.contractOwner();
+        newOwner.should.be.equal(user);
+    });
+
+    it('(ET) transfer ownership by user', async function() {
+        await expectThrow(voteFactory.transferOwnership(creator, {from: user}));
+    });
+
+    it('vote initial: change question by creator', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.setQuestion(0, question1, {from: creator});
+        var vote = await voteFactory.voteQuestion(0);
+        vote.should.be.equal(question1);
+    });
+
+    it('vote initial: change question by user', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await expectThrow(voteFactory.setQuestion(0, question1, {from: user}));
+    });
+
+    it('(ET) vote started: change question by creator', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await expectThrow(voteFactory.setQuestion(0, question1, {from: creator}));
+    });
+
+    it('vote initial: change answer by creator', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.setAnswer(0, 0, answer1, {from: creator});
+        var answer = await voteFactory.voteAnswer(0, 0);
+        answer.should.be.equal(answer1);
+    });
+
+    it ('(ET) vote started: change answer by creator', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await expectThrow(voteFactory.setAnswer(0, 0, answer1, {from: creator}));
+    });
+
+    it('voting', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        var state = await voteFactory.voteState(0);
+        one.should.be.equal(state.toString());
+
+        await voteFactory.cast(0, 1, {from: user});
+        var i = await voteFactory.voterAnswerId(user, 0);
+        one.should.be.equal(i.toString());
+        var addr = await voteFactory.voterAddress(0, 0);
+        addr.should.be.equal(user);
+    });
+    
+    it('unchanged voting', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await voteFactory.cast(0, 1, {from: user});
+        await voteFactory.cast(0, 0, {from: user});
+        var i = await voteFactory.voterAnswerId(user, 0);
+        one.should.be.equal(i.toString());
+    });
+
+    it('vote started: answer0 - 2 vote, answer1 - 1 vote', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await voteFactory.cast(0, 0, {from: user2});
+        await voteFactory.cast(0, 0, {from: user3});
+        await voteFactory.cast(0, 1, {from: user4});
+        var res = await voteFactory.results(0);
+        res.should.be.equal(answer0);
+    });
+
+    it('vote started: answer0 - 1 vote, answer1 - 1 vote', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await voteFactory.cast(0, 0, {from: user2});
+        await voteFactory.cast(0, 1, {from: user4});
+        var res = await voteFactory.results(0);
+        res.should.be.equal(multipleLiders);
+    });
+
+    it('vote started: user vote only one', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await voteFactory.cast(0, 0, {from: user2});
+        await voteFactory.cast(0, 0, {from: user3});
+        await voteFactory.cast(0, 1, {from: user4});
+        await voteFactory.cast(0, 1, {from: user4});
+        await voteFactory.cast(0, 1, {from: user4});
+        var res = await voteFactory.results(0);
+        res.should.be.equal(answer0);
+    });
+
+    it('vote started: vote only one', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await voteFactory.cast(0, 0, {from: user});
+        var res0 = await voteFactory.voterAnswerId(user, 0);
+        zero.should.be.equal(res0.toString());
+        await voteFactory.cast(0, 1, {from: user});
+        var res = await voteFactory.voterAnswerId(user, 0);
+        zero.should.be.equal(res.toString());
+    });
+
+    it('several vote', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await voteFactory.addAnswer(0, answer1, {from: creator});
+        await voteFactory.startVote(0, {from: creator});
+        await voteFactory.cast(0, 0, {from: user});
+        var res0 = await voteFactory.voterAnswerId(user, 0);
+        zero.should.be.equal(res0.toString());
+
+        await voteFactory.createVote(question1, {from: creator});
+        await voteFactory.addAnswer(1, answer0, {from: creator});
+        await voteFactory.addAnswer(1, answer1, {from: creator});
+        await voteFactory.startVote(1, {from: creator});
+        await voteFactory.cast(1, 1, {from: user});
+        var res1 = await voteFactory.voterAnswerId(user, 1);
+        one.should.be.equal(res1.toString());
+    });
+
+    it('(ET) vote initial: get result', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await expectThrow(voteFactory.results(0));
+    });
+
+    it('(ET) vote initial: voting', async function() {
+        await voteFactory.createVote(question0, {from: creator});
+        await voteFactory.addAnswer(0, answer0, {from: creator});
+        await expectThrow(voteFactory.cast(0, 0, {from: user}));
     });
 });
